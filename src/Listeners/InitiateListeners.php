eb814b90ee579d6768f3f6df143a411eb9084265
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-27 18:06:01 +0200

    Improved listeners

--- src/Listeners/InitiateListeners.php
+++ src/Listeners/InitiateListeners.php
@@ -2,14 +2,18 @@
 
 namespace VitesseCms\Import\Listeners;
 
-use Phalcon\Events\Manager;
+use VitesseCms\Core\Interfaces\InitiateListenersInterface;
+use VitesseCms\Core\Interfaces\InjectableInterface;
+use VitesseCms\Import\Listeners\Admin\AdminMenuListener;
+use VitesseCms\Import\Listeners\Fields\ImportFieldImageListener;
+use VitesseCms\Import\Listeners\Fields\ImportFieldPriceListener;
 
-class InitiateListeners
+class InitiateListeners implements InitiateListenersInterface
 {
-    public static function setListeners(Manager $eventsManager): void
+    public static function setListeners(InjectableInterface $di): void
     {
-        $eventsManager->attach('FieldImage', new ImportFieldImageListener());
-        $eventsManager->attach('FieldPrice', new ImportFieldPriceListener());
-        $eventsManager->attach('adminMenu', new AdminMenuListener());
+        $di->eventsManager->attach('FieldImage', new ImportFieldImageListener());
+        $di->eventsManager->attach('FieldPrice', new ImportFieldPriceListener());
+        $di->eventsManager->attach('adminMenu', new AdminMenuListener());
     }
 }
