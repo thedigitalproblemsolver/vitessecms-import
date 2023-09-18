ecf10439ea1018cd4c5f7bf892f53d0c056df5ff
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2023-09-18 09:53:30 +0200

    Fixed current migrations

--- src/Migrations/Migration_20210503.php
+++ src/Migrations/Migration_20210503.php
@@ -1,71 +1,49 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace VitesseCms\Import\Migrations;
 
-use VitesseCms\Cli\Services\TerminalServiceInterface;
-use VitesseCms\Configuration\Services\ConfigServiceInterface;
-use VitesseCms\Content\Repositories\ItemRepository;
-use VitesseCms\Datafield\Repositories\DatafieldRepository;
-use VitesseCms\Datagroup\Repositories\DatagroupRepository;
-use VitesseCms\Import\Repositories\ImportDatafieldRepository;
-use VitesseCms\Import\Repositories\ImportTypeRepository;
-use VitesseCms\Import\Repositories\RepositoryCollection;
-use VitesseCms\Install\Interfaces\MigrationInterface;
-use VitesseCms\Language\Repositories\LanguageRepository;
+use stdClass;
+use VitesseCms\Database\AbstractMigration;
+use VitesseCms\Import\Enum\ImportTypeEnum;
 
-class Migration_20210503 implements MigrationInterface
+class Migration_20210503 extends AbstractMigration
 {
-    /**
-     * @var RepositoryCollection
-     */
-    private $repository;
-
-    public function __construct()
-    {
-        $this->repository = new RepositoryCollection(
-            new ImportTypeRepository(),
-            new LanguageRepository(),
-            new DatagroupRepository(),
-            new ItemRepository(),
-            new ImportDatafieldRepository(),
-            new DatafieldRepository()
-        );
-    }
-
-    public function up(
-        ConfigServiceInterface $configService,
-        TerminalServiceInterface $terminalService
-    ): bool
+    public function up(): bool
     {
         $result = true;
-        if (!$this->parseImports($terminalService)) :
+        if (!$this->parseImports()) :
             $result = false;
         endif;
 
         return $result;
     }
 
-    private function parseImports(TerminalServiceInterface $terminalService): bool
+    private function parseImports(): bool
     {
         $result = true;
-        $importTypes = $this->repository->importType->findAll(null, false);
+        $importTypeRepository = $this->eventsManager->fire(ImportTypeEnum::GET_REPOSITORY->value, new stdClass());
+        $importTypes = $importTypeRepository->findAll(null, false);
 
         $search = ['Modules\\'];
         $replace = ['VitesseCms\\'];
         while ($importTypes->valid()):
             $importType = $importTypes->current();
             $type = str_replace($search, $replace, $importType->getType());
-            if (substr($type, 0, 11) === 'VitesseCms\\') :
+            if (str_starts_with($type, 'VitesseCms\\')) :
                 $importType->setType($type)->save();
             else :
-                $terminalService->printError('wrong type "' . $type . '" for importType "' . $importType->getNameField() . '"');
+                $this->terminalService->printError(
+                    'wrong type "' . $type . '" for importType "' . $importType->getNameField() . '"'
+                );
                 $result = false;
             endif;
 
             $importTypes->next();
         endwhile;
 
-        $terminalService->printMessage('ImportType type repaired');
+        $this->terminalService->printMessage('ImportType type repaired');
 
         return $result;
     }
