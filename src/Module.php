6e62eb14a706170dd24c1447bfbc854b2c50d3bc
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2023-11-25 22:29:45 +0100

    Fixed repositorycollections

--- src/Module.php
+++ src/Module.php
@@ -1,16 +1,18 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace VitesseCms\Import;
 
+use Phalcon\Di\DiInterface;
 use VitesseCms\Content\Repositories\ItemRepository;
 use VitesseCms\Core\AbstractModule;
 use VitesseCms\Datafield\Repositories\DatafieldRepository;
 use VitesseCms\Datagroup\Repositories\DatagroupRepository;
 use VitesseCms\Import\Repositories\ImportDatafieldRepository;
-use VitesseCms\Import\Repositories\RepositoryCollection;
 use VitesseCms\Import\Repositories\ImportTypeRepository;
+use VitesseCms\Import\Repositories\RepositoryCollection;
+use VitesseCms\Language\Models\Language;
 use VitesseCms\Language\Repositories\LanguageRepository;
-use Phalcon\Di\DiInterface;
 
 class Module extends AbstractModule
 {
@@ -18,13 +20,16 @@ class Module extends AbstractModule
     {
         parent::registerServices($di, 'Import');
 
-        $di->setShared('repositories', new RepositoryCollection(
-            new ImportTypeRepository(),
-            new LanguageRepository(),
-            new DatagroupRepository(),
-            new ItemRepository(),
-            new ImportDatafieldRepository(),
-            new DatafieldRepository()
-        ));
+        $di->setShared(
+            'repositories',
+            new RepositoryCollection(
+                new ImportTypeRepository(),
+                new LanguageRepository(Language::class),
+                new DatagroupRepository(),
+                new ItemRepository(),
+                new ImportDatafieldRepository(),
+                new DatafieldRepository()
+            )
+        );
     }
 }
